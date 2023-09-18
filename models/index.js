e4d7d80d97625e32cd470a9d16dcb52c2fad839a
Spencer <haze4man@gmail.com>
2023-09-17 19:38:38 -0500

    test

--- models/index.js
+++ models/index.js
@@ -1,27 +1,27 @@
-//importing modules
-const { Sequelize, DataTypes } = require("sequelize");
+// //importing modules
+// const { Sequelize, DataTypes } = require("sequelize");
 
-const sequelize = new Sequelize({
-  database: process.env.DB,
-  username: process.env.USER,
-  password: process.env.PASSWORD,
-  host: process.env.HOST,
-  port: process.env.PORT,
-  dialect: "postgres"
-});
+// const sequelize = new Sequelize({
+//   database: process.env.DB,
+//   username: process.env.USER,
+//   password: process.env.PASSWORD,
+//   host: process.env.HOST,
+//   port: process.env.PORT,
+//   dialect: "postgres"
+// });
 
-// checking if connection is done
-sequelize.authenticate().catch((err) => {
-  console.log(err);
-});
+// // checking if connection is done
+// sequelize.authenticate().catch((err) => {
+//   console.log(err);
+// });
 
-const db = {};
-db.Sequelize = sequelize;
-db.sequelize = sequelize;
+// const db = {};
+// db.Sequelize = sequelize;
+// db.sequelize = sequelize;
 
-//connecting to model
-db.money_in = require("./money_in.model")(sequelize, DataTypes);
-db.money_out = require("./money_out.model")(sequelize, DataTypes);
+// //connecting to model
+// db.money_in = require("./money_in.model")(sequelize, DataTypes);
+// db.money_out = require("./money_out.model")(sequelize, DataTypes);
 
-//exporting the module
-module.exports = db;
+// //exporting the module
+// module.exports = db;
