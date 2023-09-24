dc497d37c0d872ea42f192fbb3e5c18b7f1a6158
Spencer <haze4man@gmail.com>
2023-09-24 11:53:53 -0600

    restarting

--- models/index.js
+++ models/index.js
@@ -1,25 +1,25 @@
-//importing modules
-const { Sequelize, DataTypes } = require("sequelize");
+// //importing modules
+// const { Sequelize, DataTypes } = require("sequelize");
 
-const sequelize = new Sequelize(
-  `postgres://postgres:123456@localhost:5432/tracker_db`,
-  { dialect: "postgres" }
-);
+// const sequelize = new Sequelize(
+//   `postgres://postgres:123456@localhost:5432/tracker_db`,
+//   { dialect: "postgres" }
+// );
 
-//checking if connection is done
-sequelize
-  .authenticate()
-  .catch((err) => {
-    console.log(err);
-  });
+// //checking if connection is done
+// sequelize
+//   .authenticate()
+//   .catch((err) => {
+//     console.log(err);
+//   });
 
-const db = {};
-db.Sequelize = Sequelize;
-db.sequelize = sequelize;
+// const db = {};
+// db.Sequelize = Sequelize;
+// db.sequelize = sequelize;
 
-//connecting to model
-db.money_in = require("./money_in.model")(sequelize, DataTypes);
-db.money_out = require("./money_out.model")(sequelize, DataTypes);
+// //connecting to model
+// db.money_in = require("./money_in.model")(sequelize, DataTypes);
+// db.money_out = require("./money_out.model")(sequelize, DataTypes);
 
-//exporting the module
-module.exports = db;
+// //exporting the module
+// module.exports = db;
